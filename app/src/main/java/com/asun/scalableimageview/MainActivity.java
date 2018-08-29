98b30bf76860d6b9b29d39a0f5362415932026bb
asundev <arsun12138@gmail.com>
2018-08-29 21:55:25 +0800

    提交代码

--- app/src/main/java/com/asun/scalableimageview/MainActivity.java
+++ app/src/main/java/com/asun/scalableimageview/MainActivity.java
@@ -3,11 +3,15 @@ package com.asun.scalableimageview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.asun.library.ScalableImageView;
+
 public class MainActivity extends AppCompatActivity {
 
+    ScalableImageView mScalableImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mScalableImageView = findViewById(R.id.view);
     }
 }
